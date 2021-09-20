a04545c98b807c774f4b2f165bb3510c56fc9e4f
mohamedissack <mohamed.abdi@student.moringashool.com>
2021-09-20 23:58:00 +0300

    modify js  section

--- java.js
+++ java.js
@@ -10,3 +10,53 @@ function GetPizza(name, size, crust, topping, total) {
     this.topping = topping;
     this.total = total;
 };
+
+$(document).ready(function () {
+    $("button.proceed").click(function (event) {
+
+        let pName = $(".name option:selected").val();
+        let pSize = $("#size option:selected").val();
+        let pCrust = $("#crust option:selected").val();
+        let pTopping = [];
+
+        $.each($("input[name='toppings']:checked"), function () {
+            pTopping.push($(this).val());
+        })
+
+        switch (pSize) {
+            case "0":
+                price = 0;
+                break;
+            case 'large':
+                price = 1500;
+                break;
+            case "medium":
+                price = 950;
+                break;
+            case "small":
+                price = 700;
+            default:
+                console.log("error");
+        }
+
+        switch (pCrust) {
+            case "0":
+                c_price = 0;
+                break;
+            case "Crispy":
+                c_price = 300;
+                break;
+            case "Stuffed":
+                c_price = 200;
+                break;
+            case "Gluten-free":
+                c_price = 250;
+                break;
+            case "Flatbread":
+                c_price = 300;
+            case "Focaccia":
+                c_price = 200;
+                break;
+            default:
+                console.log("error");
+        }
